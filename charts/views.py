14bc6f05d0d903c237a1e2b8c67af8ce5e4be031
ifredysiguantay <siguantayfredy@gmail.com>
2021-05-27 16:22:12 -0600

    aplicacion de charts

---
+++ charts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from mysite.core.models import City
+
+def pie_chart(request):
+    labels = []
+    data = []
+
+    queryset = City.objects.order_by('-population')[:5]
+    for city in queryset:
+        labels.append(city.name)
+        data.append(city.population)
+
+    return render(request, 'pie_chart.html', {
+        'labels': labels,
+        'data': data,
+    })
