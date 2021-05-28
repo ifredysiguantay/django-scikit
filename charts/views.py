3ecca48f01a85dd985049bc5461f466d6601d57f
ifredyorlando <tooheyred@gmail.com>
2021-05-27 22:23:04 -0600

    Forecast de datos

--- charts/views.py
+++ charts/views.py
@@ -1,37 +1,45 @@
 from django.shortcuts import render
 import numpy as np 
 from sklearn.linear_model import LinearRegression
-
-
+from django.db import connections
+from sklearn.preprocessing import PolynomialFeatures
+cursor = connections['default'].cursor()
 def pie_chart(request):
-    labels = []
-    data = []
+    if request.method == 'POST':
+        labels = []
+        data = []
+        data_request = request.POST.get('data')
+        cursor.execute("select table_one.id from table_one where nombre_producto ='{0}'".format(data_request))
+        id_search = cursor.fetchall()
+        
+        id_search = id_search[0][0]
+        cursor.execute("select unidades_vendidas,numero_semana from table_three where product_name = '{0}'".format(id_search))
 
-    cursor.execute("select table_one.precio_unitario from table_one where nombre_producto ='{0}'".format(data))
-    id_search = cursor.fetchall()
-    id_search = id_search[0][0]
+        data = cursor.fetchall()
 
-    cursor.execute("select unidades_vendidas,numero_semana from table_three where product_name = '{0}'".format(id_search))
+        x_arr,y_arr = [],[]
 
-    data = cursor.fetchall()
+        for a,b in data:
+            tmp = b.split('-')
+            x_arr.append(a)
+            y_arr.append(tmp[1])
 
-    x_arr y_arr = [],[]
+        x = np.array(x_arr).reshape((-1,1))
+        y = np.array(y_arr)
 
-    for a,b in data:
-        x_arr.append(a[0])
-        y_arr.append(b[0])
+        model = LinearRegression()
 
-    x = np.array(x_arr).reshape((-1,1))
-    y = np.array(y_arr).
+     
 
-    model = LinearRegression()
-
-    model.fit(x,y)
+        x_ = PolynomialFeatures(degree=2, include_bias=False).fit_transform(x)
+        model = LinearRegression().fit(x_, y)
 
 
-    y_pred = model.predict(x)
+        y_pred = model.predict(x_)
+        return render(request, 'pie_chart.html', {
+            'labels': y_arr,
+            'data': list(y_pred),
+        })
+    else:
+        return render(request,'form_action.html')
 
-    return render(request, 'pie_chart.html', {
-        'labels': y_arr,
-        'data': y_pred,
-    })
