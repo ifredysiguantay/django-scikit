0b67e4cfd5a349d15ca83d1e3dc70bdfaef7860c
ifredysiguantay <siguantayfredy@gmail.com>
2021-05-28 14:14:53 -0600

    Refacturacion del codigo para la prediccion de ventas

--- charts/views.py
+++ charts/views.py
@@ -1,44 +1,79 @@
 from django.shortcuts import render
-import numpy as np 
-from sklearn.linear_model import LinearRegression
+import numpy as np
 from django.db import connections
-from sklearn.preprocessing import PolynomialFeatures
+np.set_printoptions(threshold=np.inf)
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import warnings
+from django.http import HttpResponse, HttpResponseNotFound
+warnings.filterwarnings(action="ignore", module="sklearn", message="^internal gelsd")
 cursor = connections['default'].cursor()
+
+def linearizer(lowerLimit, upperLimit):
+    upperLimit += 1
+    out = []
+    while (lowerLimit < upperLimit):
+        out.append(lowerLimit)
+        lowerLimit += 1
+    return out 
+
+
 def pie_chart(request):
     if request.method == 'POST':
-        labels = []
-        data = []
+        labels_chart = []
+        data_chart = []
+        sales_quantity = []
+
         data_request = request.POST.get('data')
-        cursor.execute("select table_one.id from table_one where nombre_producto ='{0}'".format(data_request))
+        cursor.execute("select table_one.id,table_one.precio_unitario from table_one where nombre_producto ='{0}'".format(data_request))
         id_search = cursor.fetchall()
         
+        
+        if(not len(id_search)):
+            return HttpResponseNotFound('<h1>Producto no encontrado</h1>')
+
+        price_product = id_search[0][1]
         id_search = id_search[0][0]
-        cursor.execute("select unidades_vendidas,numero_semana from table_three where product_name = '{0}'".format(id_search))
+        
+        
+        cursor.execute("select unidades_vendidas from table_three where product_name = '{0}'".format(id_search))
 
         data = cursor.fetchall()
+        input_data = pd.DataFrame(list(data))
 
-        x_arr,y_arr = [],[]
+        y = input_data.iloc[:,0]
+        length_y = len(y.index)
 
-        for a,b in data:
-            tmp = b.split('-')
-            x_arr.append(a)
-            y_arr.append(tmp[1])
+        x = []
 
-        x = np.array(x_arr).reshape((-1,1))
-        y = np.array(y_arr)
+        x = linearizer(0,(length_y -1))
+
+        x = pd.DataFrame(x)
 
         model = LinearRegression()
+        model.fit(x,y)
 
-     
+        positionToPredict =  request.POST.get('weeks')
 
-        x_ = PolynomialFeatures(degree=2, include_bias=False).fit_transform(x)
-        model = LinearRegression().fit(x_, y)
+        positionToPredict = int(positionToPredict)
 
+        new_data = linearizer(length_y,(positionToPredict + length_y))
 
-        y_pred = model.predict(x_)
+        new_data = pd.DataFrame(new_data)
+        prediction = model.predict(new_data)
+    
+        
+        for a in range(1,len(prediction)):
+            labels_chart.append(a)
+        for b in list(prediction):
+            data_chart.append(price_product*round(b))
+            sales_quantity.append(round(b))
+        sales_quantity.pop()
         return render(request, 'pie_chart.html', {
-            'labels': y_arr,
-            'data': list(y_pred),
+            'labels':labels_chart,
+            'data': data_chart,
+            'sales_quantity':sales_quantity,
+            'price_product':price_product
         })
     else:
         return render(request,'form_action.html')
