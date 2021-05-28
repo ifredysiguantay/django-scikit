2fb66b6c088ca29ba62d6e85e01001ce8334ebbc
ifredyorlando <tooheyred@gmail.com>
2021-05-27 21:21:00 -0600

    Modificacion de archivos complementarios del proyecto

--- charts/views.py
+++ charts/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
-from mysite.core.models import City
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+
 
 def pie_chart(request):
     labels = []
     data = []
 
-    queryset = City.objects.order_by('-population')[:5]
-    for city in queryset:
-        labels.append(city.name)
-        data.append(city.population)
+    cursor.execute("select table_one.precio_unitario from table_one where nombre_producto ='{0}'".format(data))
+    id_search = cursor.fetchall()
+    id_search = id_search[0][0]
+
+    cursor.execute("select unidades_vendidas,numero_semana from table_three where product_name = '{0}'".format(id_search))
+
+    data = cursor.fetchall()
+
+    x_arr y_arr = [],[]
+
+    for a,b in data:
+        x_arr.append(a[0])
+        y_arr.append(b[0])
+
+    x = np.array(x_arr).reshape((-1,1))
+    y = np.array(y_arr).
+
+    model = LinearRegression()
+
+    model.fit(x,y)
+
+
+    y_pred = model.predict(x)
 
     return render(request, 'pie_chart.html', {
-        'labels': labels,
-        'data': data,
+        'labels': y_arr,
+        'data': y_pred,
     })
--- django_scikit/settings.py
+++ django_scikit/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'django_scikit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': 'prueba_tecnica',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root'
+    },
 }
 
 
